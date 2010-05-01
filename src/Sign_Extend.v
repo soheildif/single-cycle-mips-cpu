d891d06855243ceb4747a4044b946fbac4834c3b
hckirmizi <hckirmizi@6c8f87ed-6bdf-c87e-259f-01baa1f1a252>
2010-05-01 07:04:37 +0000

    Sign_Extend.v is done.

---
+++ src/Sign_Extend.v
@@ -0,0 +1,23 @@
+﻿/* =============================================================================
+ *
+ * Name           : Sign_Extend.v
+ * Author         : Hakki Caner Kirmizi
+ * Date           : 2010-5-1
+ * Description    : A module that gets an 16-bit signed integer as input and
+ *					extends it to a 32-bit signed integer
+ *                  
+ * =============================================================================
+*/
+
+module Signed_Extend(
+    .data_in,
+    .data_out
+);
+
+input	[15:0]	data_in;
+output	[31:0]	data_out;
+
+// n{m} form will generate a vector of bits with the pattern m repeated n times
+assign data_out = { 16{data_in[31]}, data_in };
+
+endmodule
