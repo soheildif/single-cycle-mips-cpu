cf29fa0959369e2d3f00b2c5504e807368412c8e
hckirmizi <hckirmizi@6c8f87ed-6bdf-c87e-259f-01baa1f1a252>
2010-05-01 06:14:47 +0000

    Adder.v is done.

--- src/Adder.v
+++ src/Adder.v
@@ -1,12 +1,12 @@
-// ===================================================
+// =============================================================================
 // 
 // Name           : Adder.v
 // Author         : Hakki Caner Kirmizi
 // Date           : 2010-4-30
-// Description    : A module that sums up two 32-bit 
-//                  input and assigns to 32-bit output
+// Description    : A module that sums up two 32-bit input and assigns to 
+// 					32-bit output
 //                  
-// ===================================================
+// =============================================================================
 
 module Adder(
     .data1_in,
@@ -14,11 +14,13 @@ module Adder(
     .data_out
 );
 
-// Interface
+
 input   [31:0]  data1_in;
 input   [31:0]  data2_in;
-output  [31:0]  aaadata_out;
+output  [31:0]  data_out;
+
 
 assign data_out = data1_in + data2_in;
+
   
 endmodule
