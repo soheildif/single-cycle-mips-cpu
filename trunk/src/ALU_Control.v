32ca0a7ddc0b8885629267d59bbd4deab1e3baad
frostmoron <frostmoron@6c8f87ed-6bdf-c87e-259f-01baa1f1a252>
2010-05-02 08:23:35 +0000

    Modified Control.v & ALU_Control.v (because the length of ALUOp of the example in the slide isn't enough to support the required operations)

--- trunk/src/ALU_Control.v
+++ trunk/src/ALU_Control.v
@@ -13,34 +13,62 @@
  * -----------------------
  * ALUOp		Function		ALUCtrl		Instr. Opcode
  * -----		--------		-------		-------------
- * 00			xxxxxx			010			lw, sw
- * 01			xxxxxx			110			beq
- * 10			100000			010			add
- * 10			100010			110			sub
- * 10			100100			000			and
- * 10			100101			001			or
- * 10			101010			111			slt
+ * 010			xxxxxx			010			lw, sw, addi (same as add)
+ * 011			xxxxxx			110			beq (same as sub)
+ * 000                  xxxxxx                  000                     andi (same as and)
+ * 001                  xxxxxx                  001                     ori (same as or)
+ * 100			100000			010			add
+ * 100			100010			110			sub
+ * 100			100100			000			and
+ * 100			100101			001			or
+ * 100			101010			111			slt
+ * 
  * 
  * Note: Adding ‘addi, andi, ori’ does not require any changes in the given 
  * ALU because they are already covered in their R-format counterparts. However, 
  * we assume that 6-bit function code which is input to the ALU control circuit 
  * will be correctly generated at the main control circuit.
  * 
+ * CCode (10/5/2(SUN)): we need to extend ALUOp to 3-bits since andi & ori 
+ *                      doesn't have funct field in their instructions.
+ *
  * =============================================================================
 */
 
 module ALU_Control(
-	funct,
-	ALUOp,
-	ALUCtrl
-);
-
-input	[15:0]	funct;		// 16-bit represents instruction's 'address' part
-input	[1:0]	ALUOp;		// 2-bit comes from Control
-output	[2:0]	ALUCtrl;	// 3-bit control signal represents the operation type, e.g. add, or etc..
-
-assign ALUCtrl[2] = ~ALUOp[0] | (~ALUOp[1] & funct[1]);
-assign ALUCtrl[1] = ALUOp[1] | funct[2];
-assign ALUCtrl[0] = ALUOp[1] & (funct[0] & funct[3]);
-
+		   funct,
+		   ALUOp,
+		   ALUCtrl
+		   );
+   
+   input [5:0]	funct;		// 6-bit represents instruction's 'address' part
+   input [1:0] 	ALUOp;		// 2-bit comes from Control
+   output [2:0] ALUCtrl;	// 3-bit control signal represents the operation type, e.g. add, or etc..
+   
+   reg [2:0] 	ALUCtrl;
+      
+   always @(ALUOp or funct)
+     case(ALUOp)
+       3'b010:
+	 ALUCtrl = 3'b010;
+       3'b011:
+	 ALUCtrl = 3'b110;
+       3'b000:
+	 ALUCtrl = 3'b000;
+       3'b001:
+	 ALUCtrl = 3'b001;
+       3'b100:
+	 case(funct)
+	   6'b100000:
+	     ALUCtrl = 3'b010;
+	   6'b100010:
+	     ALUCtrl = 3'b110;
+	   6'b100100:
+	     ALUCtrl = 3'b000;
+	   6'b100101:
+	     ALUCtrl = 3'b001;
+	   6'b101010:
+	     ALUCtrl = 3'b111;
+	 endcase // case (funct)
+     endcase // case (ALUOp)
 endmodule
