719ec41b4294b7be0c4af4441ef369e11200a1cc
frostmoron <frostmoron@6c8f87ed-6bdf-c87e-259f-01baa1f1a252>
2010-05-02 08:23:35 +0000

    Modified Control.v & ALU_Control.v (because the length of ALUOp of the example in the slide isn't enough to support the required operations)

--- src/Control.v
+++ src/Control.v
@@ -12,8 +12,8 @@ module Control(
    
    input [5:0]  opcode;
    output 	RegDst, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
-   output [1:0] ALUOp;
-   reg [8:0] 	out;
+   output [2:0] ALUOp;
+   reg [9:0] 	out;
    
    assign RegDst = out[8];
    assign Branch = out[7];
@@ -22,24 +22,24 @@ module Control(
    assign MemWrite = out[4];
    assign ALUSrc = out[3];
    assign RegWrite = out[2];
-   assign ALUOP = out[1:0];
+   assign ALUOP = out[2:0];
    
    always @(opcode)
      case(opcode)
-       6'b000000:
-	 out = ;
-       6'b001000:
-	 out = ;
-       6'b001100:
-	 out = ;
-       6'b001101:
-	 out = ;
-       6'b101011:
-	 out = ;
-       6'b100011:
-	 out=;
-       6'b000100:
-	 out=;
+       6'b000000: //R-type
+	 out = 10'b1000001100;
+       6'b001000: //addi
+	 out = 10'b0000011010;
+       6'b001100: //andi
+	 out = 10'b0000011000;
+       6'b001101: //ori
+	 out = 10'b0000011001;
+       6'b101011: //sw
+	 out = 10'b0000110010;
+       6'b100011: //lw
+	 out = 10'b0011011010;
+       6'b000100: //beq
+	 out = 10'b0100000011;
      endcase // case (opcode)
    
 endmodule
